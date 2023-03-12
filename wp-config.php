cfd553c69a30cdae10317b9a27937742cb8551b8
ocetot <perisjara@gmail.com>
2023-03-12 02:46:13 -0300

    primer wordpress

--- wp-config.php
+++ wp-config.php
@@ -37,6 +37,8 @@ define( 'DB_CHARSET', 'utf8mb4' );
 /** The database collate type. Don't change this if in doubt. */
 define( 'DB_COLLATE', '' );
 
+define('WP_ENVIRONMENT_TYPE', 'local');
+
 /**#@+
  * Authentication unique keys and salts.
  *
